1f88aa5f6174ff4dcff2d48deaf2f438af446d8e
Prabhat Dehariya <prabhatdehariya20@gmail.com>
2026-05-24 04:09:36 +0530

    Create half_adder_tb.v

---
+++ half_adder_tb.v
@@ -0,0 +1,16 @@
+module tb_half_adder;
+reg A, B;
+wire Sum, Carry;
+half_adder uut (
+.A(A),
+.B(B),
+.Sum(Sum),
+.Carry(Carry)
+);
+Initial begin
+A=0; B=0; #10;
+A=0; B=1; #10;
+A=1; B=0; #10;
+A=1; B=1; #10;
+End
+endmodule
